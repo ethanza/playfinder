3ac18858e907521e4c8f1c7f6dc1cc144bcf9f60
ethan.alexander <ethan.alexander@digioutsource.com>
2022-03-14 11:21:14 +0200

    adding the store

--- src/app/search-slot/search-slot.component.ts
+++ src/app/search-slot/search-slot.component.ts
@@ -5,38 +5,44 @@ import { Store, select } from "@ngrx/store";
 import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: "app-search-slot",
-  templateUrl: "./search-slot.component.html",
-  styleUrls: ["./search-slot.component.scss"],
+    selector: "app-search-slot",
+    templateUrl: "./search-slot.component.html",
+    styleUrls: ["./search-slot.component.scss"],
 })
 export class SearchSlotComponent implements OnInit {
-  results$: Observable<ResultData[]>;
-  result$: Observable<ResultData>;
+    results$: Observable<ResultData[]>;
+    result$: Observable<ResultData>;
 
-  public result: ResultData;
+    public result: ResultData;
 
-  constructor(
-    private store: Store<{ slots: ResultData[] }>,
-    private route: ActivatedRoute,
-    private router: Router
-  ) {
-    this.results$ = this.store.pipe(select("slots"));
-    this.store.dispatch({
-      type: "[Slots] Get One Slot",
-      result_id: this.route.snapshot.params.id,
-    });
-  }
+    constructor(
+        private store: Store<{ slots: ResultData[] }>,
+        private route: ActivatedRoute,
+        private router: Router
+    ) {
+        this.results$ = this.store.pipe(select("slots"));
+        this.store.dispatch({
+            type: "[Slots] Get One Slot",
+            result_id: this.route.snapshot.params.id,
+        });
+    }
 
-  ngOnInit(): void {
-    this.results$.subscribe((results) => {
-      if (results.length !== 0) {
-        this.result = results.find(
-          (result) => result.id == this.route.snapshot.params.id
-        );
-        // else dispatch event to get the results from API
-      } else {
+    ngOnInit(): void {
+        this.results$.subscribe((results) => {
+            if (results.length !== 0) {
+                this.result = results.find(
+                    (result) => result.id == this.route.snapshot.params.id
+                );
+                // else dispatch event to get the results from API
+            } else {
+                this.router.navigate(["/"]);
+            }
+        });
+    }
+
+    public bookCourt(): void {
+        alert("court booked");
         this.router.navigate(["/"]);
-      }
-    });
-  }
+        //TODO add functionality to BE to remove and update the court status
+    }
 }
